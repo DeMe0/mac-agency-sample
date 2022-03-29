f3e3de543a1be0493efa9df2ad1d014a8dff4dfa
Anthony DeMeo <anthonydemeo@Anthonys-MacBook-Pro.local>
2022-03-28 20:26:50 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,284 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <nav
+        class="navbar navbar-expand-xl navbar-dark bg-black"
+        aria-label="Sixth navbar example"
+      >
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="https://res.cloudinary.com/ademeo/image/upload/v1648496834/mac/mac-agency-logo_kkcw5p.jpg" />
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarsExample06"
+            aria-controls="navbarsExample06"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarsExample06">
+            <ul class="navbar-nav me-auto mb-2 mb-xl-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link"
+                  href="https://www.instagram.com/mac.agency/?hl=en"
+                >
+                  Artists
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link ">Contact Us</a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link"
+                  href="https://www.instagram.com/mac.agency/?hl=en"
+                >
+                  Instagram
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link ">Our Team</a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link"
+                  href="https://www.facebook.com/MajorArtistConcertsLLC/"
+                >
+                  Facebook
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="dropdown06"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="dropdown06">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <form>
+              <input
+                class="form-control"
+                type="text"
+                placeholder="Search"
+                aria-label="Search"
+              />
+            </form>
+          </div>
+        </div>
+      </nav>
+      <div class="container">
+        <div class="row">
+          <div class="col">Column</div>
+          <div class="col">Column</div>
+          <div class="col">Column</div>
+        </div>
+      </div>
+      {/* <!-- Carousel wrapper --> */}
+      <div
+        id="carouselVideoExample"
+        class="carousel slide carousel-fade"
+        data-mdb-ride="carousel"
+      >
+        {/* <!-- Indicators --> */}
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-mdb-target="#carouselVideoExample"
+            data-mdb-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-mdb-target="#carouselVideoExample"
+            data-mdb-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-mdb-target="#carouselVideoExample"
+            data-mdb-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+
+        {/* <!-- Inner --> */}
+        <div class="carousel-inner">
+          {/* <!-- Single item --> */}
+          <div class="carousel-item active">
+            <video class="img-fluid" autoplay loop muted>
+              <source
+                src="https://res.cloudinary.com/ademeo/video/upload/v1648512942/mac/nba-youngboy_pn16bo.mp4"
+                type="video/mp4"
+              />
+            </video>
+            <div class="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </div>
+          </div>
+
+          {/* <!-- Single item --> */}
+          <div class="carousel-item">
+            <video class="img-fluid">
+              <source
+                src="https://mdbcdn.b-cdn.net/img/video/forest.mp4"
+                type="video/mp4"
+              />
+            </video>
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+
+          {/* <!-- Single item --> */}
+          <div class="carousel-item">
+            <video class="img-fluid" autoplay loop muted>
+              <source
+                src="https://mdbcdn.b-cdn.net/img/video/Agua-natural.mp4"
+                type="video/mp4"
+              />
+            </video>
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* <!-- Inner --> */}
+
+        {/* <!-- Controls --> */}
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-mdb-target="#carouselVideoExample"
+          data-mdb-slide="prev"
         >
-          Learn React
-        </a>
-      </header>
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-mdb-target="#carouselVideoExample"
+          data-mdb-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      {/* <!-- Carousel wrapper --> */}
+      {/* <div
+        id="carouselExampleIndicators"
+        class="carousel slide carousel-fade"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <video class="img-fluid" autplay loop muted>
+              <source
+                src="https://res.cloudinary.com/ademeo/video/upload/v1648511518/mac/mac-nba-youngboy-carousel-video_vjlzln.mov"
+                type="video/mp4"
+              />
+            </video>
+          </div>
+          <div class="carousel-item">
+            <img
+              src="https://res.cloudinary.com/ademeo/image/upload/v1648490028/mac/mac-agency-logo_xfbp6w.jpg"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="https://res.cloudinary.com/ademeo/image/upload/v1648490028/mac/mac-agency-logo_xfbp6w.jpg"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div> */}
     </div>
   );
 }
