50d5a02eeae68f0a3f9ae6cf81bcf8d3b3b73bb4
Anthony DeMeo <anthonydemeo@Anthonys-MacBook-Pro.local>
2022-04-04 21:56:43 -0400

    fixed card grid. not finished but working great

--- src/components/RosterCards.js
+++ src/components/RosterCards.js
@@ -4,50 +4,71 @@ import { Card } from "react-bootstrap";
 
 const RosterCards = (props) => {
   return (
-    <div className="artistsContainer">
+    <div
+      className="artistsContainer"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <h2 id="artists">Artists</h2>
-      <div class="row">
-        <div class="col-sm-6">
-          {props.rosterData[0] &&
-            //   as long as rosterData is not an empty array, map over it
-            props.rosterData[0].information.map((artist) => (
-              <div
-                className="card text-white bg-dark mb-3"
+      <div
+        class="row"
+        style={{
+          justifyContent: "center",
+        }}
+      >
+        {props.rosterData[0] &&
+          //   as long as rosterData is not an empty array, map over it
+          props.rosterData[0].information.map((artist) => (
+            <div
+              className="card text-white bg-dark mb-3"
+              id="cardz"
+              style={{
+                width: "18rem",
+                padding: "5px 14px 14px 15px",
+                margin: "20px",
+              }}
+            >
+              <img
+                src={artist.pfp}
+                class="card-img-top bg-image hover-zoom"
+                alt="..."
                 style={{
-                  width: "18rem",
-                  padding: "5px",
-                  border: "1px solid red",
+                  width: "100%",
+                  height: "300px",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                  objectFit: "cover",
                 }}
-              >
-                <img src={artist.pfp} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h5
-                    className="card-title"
-                    //   style={{
-                    //     color: "white",
-                    //   }}
-                  >
-                    {artist.name}
-                  </h5>
-                </div>
-                <div className="card-body">
-                  <a href="#" className="card-link">
-                    <button
-                      type="button"
-                      class="btn btn-warning"
-                      style={{
-                        color: "white",
-                        textShadow: "1px 2px #303030",
-                        background: "#ECBD76",
-                      }}
-                    >
-                      Tickets
-                    </button>
-                  </a>
-                </div>
+              />
+              <div className="card-body">
+                <h5
+                  className="card-title"
+                  //   style={{
+                  //     color: "white",
+                  //   }}
+                >
+                  {artist.name}
+                </h5>
               </div>
-            ))}
-        </div>
+              <div className="card-body">
+                <a href="#" className="card-link">
+                  <button
+                    type="button"
+                    class="btn btn-warning"
+                    style={{
+                      color: "white",
+                      textShadow: "1px 2px #303030",
+                      background: "#ECBD76",
+                    }}
+                  >
+                    Tickets
+                  </button>
+                </a>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
